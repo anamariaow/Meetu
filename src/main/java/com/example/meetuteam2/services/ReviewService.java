34b4eb4cf7f1d0e42a4c4901ac2c1d210f6f4105
Antonio Taminto <taminto.a@gmail.com>
2024-04-02 12:21:13 +0200

    added ReviewService javadocs and ReviewController

--- src/main/java/com/example/meetuteam2/services/ReviewService.java
+++ src/main/java/com/example/meetuteam2/services/ReviewService.java
@@ -13,15 +13,31 @@ import java.util.Optional;
 public class ReviewService {
     @Autowired
     private ReviewRepository reviewRepository;
-
+    /**
+     * questo metodo crea una nuova Review
+     * @param review
+     * @return Review inserito
+     * @author AT
+     */
     public Review createReview(Review review){
         Review savedReview = reviewRepository.save(review);
         return savedReview;
     }
+    /**
+     * questo metodo ritorna la lista delle Review attive
+     * @return lista delle Review attive
+     * @author AT
+     */
     public List<Review> getActiveReviewList(){
         List<Review> reviewsList = reviewRepository.findAllActiveReview();
         return reviewsList;
     }
+    /**
+     * questo metodo recupera una Review partendo dal suo id
+     * @param id
+     * @return la Review trovata (se presente) oppure ritorna un Optional vuoto
+     * @author AT
+     */
     public Optional<Review> getReviewById(Long id){
         Optional<Review> reviewOptional = reviewRepository.findById(id);
         if(reviewOptional.isPresent()){
@@ -30,6 +46,13 @@ public class ReviewService {
             return Optional.empty();
         }
     }
+    /**
+     * questo metodo aggiorna i field selezionati di un User, recuperandolo attraverso l'id
+     * @param id
+     * @param review
+     * @return la Review aggiornata (se presente) oppure ritorna un Optional vuoto
+     * @author AT
+     */
     public Optional<Review> updateReview(Long id,Review review){
         Optional<Review> reviewOptional = getReviewById(id);
         if(reviewOptional.isPresent()){
@@ -44,21 +67,18 @@ public class ReviewService {
             return Optional.empty();
         }
     }
-    public Optional<Review> updateReviewRecordStatus(Long id, RecordStatusEnum recordStatusEnum){
+    /**
+     * questo metodo aggiorna lo status di una Review in deleted, recuperandolo attraverso l'id
+     * @param id
+     * @return la Review aggiornata (se presente) oppure ritorna un Optional vuoto
+     * @author AT
+     */
+    public Optional<Review> deleteReviewRecordStatus(Long id){
         Optional<Review> reviewOptional = getReviewById(id);
         if(reviewOptional.isPresent()){
-            reviewOptional.get().setRecordStatus(recordStatusEnum);
+            reviewOptional.get().setRecordStatus(RecordStatusEnum.D);
             Review savedReview = reviewRepository.save(reviewOptional.get());
             return Optional.of(savedReview);
-        }else {
-            return Optional.empty();
-        }
-    }
-    public Optional<Review> deleteReview(Long id){
-        Optional<Review> reviewOptional = getReviewById(id);
-        if(reviewOptional.isPresent()){
-            reviewRepository.deleteById(reviewOptional.get().getId());
-            return Optional.of(reviewOptional.get());
         }else {
             return Optional.empty();
         }
