96877f8624a5116d971e27f1eaceaca580d667bc
MattWorkHub <150373956+MattWorkHub@users.noreply.github.com>
2024-04-03 12:30:49 +0200

    Added Experience Controller and Service and fixed Experience Repository.

--- src/main/java/com/example/meetuteam2/repositories/ExperienceRepository.java
+++ src/main/java/com/example/meetuteam2/repositories/ExperienceRepository.java
@@ -1,18 +1,15 @@
 package com.example.meetuteam2.repositories;
 
-import com.example.meetuteam2.entities.User;
-import com.example.meetuteam2.entities.enums.RecordStatusEnum;
+import com.example.meetuteam2.entities.Experience;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
-public interface ExperienceRepository extends JpaRepository<User, Long> {
+public interface ExperienceRepository extends JpaRepository<Experience, Long> {
     @Query("SELECT e FROM Experience e WHERE recordStatus = 'A'")
-    List<User> findAllActiveExperiences();
-    @Query("SELECT e FROM Experience e WHERE recordStatus = 'A' AND id = ?1")
-    Optional<User> findActiveExperienceById(Long id);
+    List<Experience> findAllActiveExperiences();
 }
